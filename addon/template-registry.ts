b6638ad1892c08111ac54fa903b9faf32bd17f4e
Adam Baker <bakerac4@gmail.com>
2022-11-11 15:27:11 -0500

    Adding or to glint registry

--- addon/template-registry.ts
+++ addon/template-registry.ts
@@ -1,7 +1,9 @@
 import type AndHelper from 'ember-template-logical-operators-polyfill/helpers/and';
 import NotHelper from 'ember-template-logical-operators-polyfill/helpers/not';
+import OrHelper from 'ember-template-logical-operators-polyfill/helpers/or';
 
 export default interface EmberTemplateLogicalOperatorsPolyfillRegistry {
   and: typeof AndHelper;
   not: typeof NotHelper;
+  or: typeof OrHelper;
 }
